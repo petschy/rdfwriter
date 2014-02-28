2b992e176d868860be64cfe5a1885aeffdeccc0e
petschy <petschy@bluewin.ch>
2014-02-28 08:32:31 +0100

    - Delete Space at beginning of line - Tidy punctuation at end of line - Add methods rightPad and leftPad for adding leading zeroes

--- src/main/java/ch/admin/nb/lod/rdfwriter/tools/StringTool.java
+++ src/main/java/ch/admin/nb/lod/rdfwriter/tools/StringTool.java
@@ -15,12 +15,37 @@ public class StringTool {
 		s = s.replaceAll(" {2,}", " ");
 		// ISBD-Interpunktion am Zeilenende löschen
 		s = s.replaceAll(" :$", "");
+		s = s.replaceAll(" ;$", "");
 		s = s.replaceAll(" /$", "");
 		s = s.replaceAll(" =$", "");
 		s = s.replaceAll(",$", "");
+		// Leerschlag an Zeilenanfang löschebn
+		s = s.replaceAll("^ ", "");
 		// Normalisieren NFC
 		s = Normalizer.normalize(s, Normalizer.Form.NFC);
 		return s;
 		
 	}
+	
+	/**
+	 * @param str
+	 * @param length
+	 * @param c
+	 * @return
+	 */
+	public static String rightPad(String str, Integer length, char c){
+		return str + String.format("%" + (length - str.length()) + "s", "").replace(" ", String.valueOf(c));
+		
+	}
+	/**
+	 * @param str
+	 * @param length
+	 * @param c
+	 * @return
+	 */
+	public static String leftPad(String str, Integer length, char c){
+		return String.format("%" + (length - str.length()) + "s", "").replace(" ", String.valueOf(c)) + str;
+		
+	}
 }
+
