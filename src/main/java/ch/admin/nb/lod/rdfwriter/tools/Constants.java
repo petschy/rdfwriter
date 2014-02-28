160f10a53c70166239b0b41c948bcaaa9ced4916
petschy <petschy@bluewin.ch>
2014-02-28 08:25:30 +0100

    - Add constant for Marc21 field array KIM-Relations - Add constant for Namespaces bibo, rda, isbd, loc

--- src/main/java/ch/admin/nb/lod/rdfwriter/tools/Constants.java
+++ src/main/java/ch/admin/nb/lod/rdfwriter/tools/Constants.java
@@ -19,6 +19,8 @@ public class Constants {
 	public static final String[] KIM_CREATOR = { "100", "110", "111", "700",
 			"710", "711" };
 
+	public static final String[] KIM_RELATION = { "770", "774", "775", "773", "776", "780", "785", "800", "810", "811", "830"};
+	
 	// Dublin Core
 	public static final String NS_DC_PREFIX = "dc";
 	public static final String NS_DCTERMS_PREFIX = "dcterms";
@@ -35,10 +37,11 @@ public class Constants {
 	public static final String NS_BIBO_PERIODICAL = "http://purl.org/ontology/bibo/Periodical";
 	public static final String NS_BIBO_ARTICLE = "http://purl.org/ontology/bibo/Article";
 	public static final String NS_BIBO_ISSUE = "http://purl.org/ontology/bibo/Issue";
+	public static final String NS_BIBO_EDITION = "http://purl.org/ontology/bibo/edition";
 
 	// Helveticat
 	// Titeldaten (helveticatBib)
-	public static final String NS_HELVETICAT_BIB = "http://nb.admin.ch/lod/helveticat/bib";
+	public static final String NS_HELVETICAT_BIB = "http://nb.admin.ch/lod/helveticat/bib/";
 	public static final String NS_HELVETICAT_BIB_PREFIX = "helveticatBib";
 	// Normdaten (helveticatAuth)
 	public static final String NS_HELVETICAT_AUTH = "http://nb.admin.ch/lod/helveticat/auth/";
@@ -59,15 +62,19 @@ public class Constants {
 	public static final String NS_NBO_HELVETICAT_WORK = "http://nb.admin.ch/lod/ontology#preferredNameForTheWork";
 
 	// RDA
+	// Elements
 	public static final String NS_RDA = "http://rdvocab.info/Elements/";
 	public static final String NS_RDA_PREFIX = "rda";
 	public static final String NS_RDA_PUBLICATION_STATEMENT = "http://rdvocab.info/Elements/publicationStatement";
 	public static final String NS_RDA_PLACE_OF_PUBLICATION = "http://rdvocab.info/Elements/placeOfPublication";
 	public static final String NS_RDA_OTHER_TITLE_INFORMATION = "http://rdvocab.info/Elements/otherTitleInformation";
-
-	// RDA Carriertype
+	// RDARelationshipsWEMI
+	public static final String NS_RDA_WEMI_PREFIX_WEMI = "rdaRelationshipsWEMI";
+	public static final String NS_RDA_WEMI_PRECEDED_BY = "http://rdvocab.info/RDARelationshipsWEMI/precededBy";
+	public static final String NS_RDA_WEMI_SUCCEEDED_BY = "http://rdvocab.info/RDARelationshipsWEMI/succeededBy";
+	// Carriertype
 	public static final String NS_RDA_CARRIERTYPE = "http://rdvocab.info/termList/RDACarrierType/";
-	public static final String NS_RDA_CARRIERTYPE_PREFIX = "rdacarriertype";
+	public static final String NS_RDA_CARRIERTYPE_PREFIX = "rdaCarriertype";
 	public static final String NS_RDA_CARRIERTYPE_UNMEDIATED = "http://rdvocab.info/termList/RDACarrierType/1044";
 	public static final String NS_RDA_CARRIERTYPE_MICROFORM = "http://rdvocab.info/termList/RDAMediaType/1002";
 	public static final String NS_RDA_CARRIERTYPE_ONLINERESOURCE = "http://rdvocab.info/termList/RDACarrierType/1018";
@@ -83,9 +90,17 @@ public class Constants {
 	public static final String NS_LIB_BRAILLE = "http://purl.org/library/BrailleBook";
 	
 	// ISBD
-	public static final String NS_ISBD = "http://iflastandards.info/ns/isbd/terms/mediatype/";
-	public static final String NS_ISBD_PREFIX = "isbdmediatype";
-	public static final String NS_ISBD_MULTIMEDIA = "http://iflastandards.info/ns/isbd/terms/mediatype/T1008";
+	// Mediatype
+	public static final String NS_ISBD_MEDIATYPE = "http://iflastandards.info/ns/isbd/terms/mediatype/";
+	public static final String NS_ISBD_MEDIATYPE_PREFIX = "isbdMediatype";
+	public static final String NS_ISBD_MEDIATYPE_MULTIMEDIA = "http://iflastandards.info/ns/isbd/terms/mediatype/T1008";
+	// Elements
+	public static final String NS_ISBD_ELEMENTS = "http://iflastandards.info/ns/isbd/elements/";
+	public static final String NS_ISBD_ELEMENTS_PREFIX = "isbd";
+	public static final String NS_ISBD_ELEMENTS_HASEXTENT = "http://iflastandards.info/ns/isbd/elements/P1053";
+	
+	// LOC
+	public static final String NS_MARC_LANGUAGES = "http://id.loc.gov/vocabulary/languages/";
 	
 	
 }
