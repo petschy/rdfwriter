a213017f6cc9dc554e599a72830d42d10d1fc4a4
petschy <petschy@bluewin.ch>
2014-04-04 09:31:11 +0200

    Major changes

--- src/main/java/ch/admin/nb/lod/rdfwriter/kim/MediaType.java
+++ src/main/java/ch/admin/nb/lod/rdfwriter/kim/MediaType.java
@@ -37,62 +37,163 @@ public class MediaType {
 		String data008 = f008.getData();
 		String data008pos21 = data008.substring(21, 22);
 
-		if (typeOfRecord == 'a') {
-			model.add(rdfSubject, DCTerms.medium, Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+		// Leader 06: Type of Record
+		switch (typeOfRecord) {
 
-			switch (bibLevel) {
+		// a - Language material
+		case 'a':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
 
-			// Monographic component part
-			case 'a':
-				break;
-			// Serial component part
-			case 'b':
-				break;
-			// Collection
-			case 'c':
-				break;
-			// Subunit
-			case 'd':
-				break;
-			// Integrating resource
-			case 'i':
-				break;
-			// Monograph/Item
-			case 'm':
-				break;
-			// Serial
-			case 's':
-				if (data008pos21.equals("m")) {
-					model.add(rdfSubject, RDF.type, Constants.NS_BIBO_SERIES);
-				} else {
-					model.add(rdfSubject, RDF.type,
-							Constants.NS_BIBO_PERIODICAL);
+			break;
+		// c - Notated music
+		case 'c':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+		// d - Manuscript notated music
+		case 'd':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+		// e - Cartographic material
+		case 'e':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_MAP);
+			break;
+		// f - Manuscript cartographic material
+		case 'f':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_MAP);
+			break;
+		// g - Projected medium
+		case 'g':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIOVISUAL);
+			break;
+		// i - Nonmusical sound recording
+		case 'i':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIODOCUMENT);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_MEDIATYPE_AUDIO);
+			break;
+		// j - Musical sound recording
+		case 'j':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIODOCUMENT);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_MEDIATYPE_AUDIO);
+			break;
+		// k - Two-dimensional nonprojectable graphic
+		case 'k':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_IMAGE);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+		// m - Computer file
+		case 'm':
 
-				}
+			break;
+		// o - Kit
+		case 'o':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIOVISUAL);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_ISBD_MEDIATYPE_MULTIMEDIA);
+			break;
+		// o - Medienkombination
+		case 'p':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIOVISUAL);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_ISBD_MEDIATYPE_MULTIMEDIA);
+			break;
+		// r - Three-dimensional artifact
+		case 'r':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+		// r - Manuscript
+		case 't':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+
+		default:
+			break;
+		}
+
+		switch (bibLevel) {
+
+		// Monographic component part
+		case 'a':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_ARTICLE);
+			break;
+		// Serial component part
+		case 'b':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_ISSUE);
+			break;
+		// Collection
+		case 'c':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_COLLECTION);
+			break;
+		// Subunit
+		case 'd':
+			break;
+		// Integrating resource
+		case 'i':
+			break;
+		// Monograph/Item
+		case 'm':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_DOCUMENT);
+			break;
+		// Serial
+		case 's':
+
+			switch (data008pos21) {
+			// # - None of the following
+			case " ":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// d - Updating database
+			case "d":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// l - Updating loose-leaf
+			case "l":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// m - Monographic series
+			case "m":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_SERIES);
+				break;
+			// n - Newspaper
+			case "n":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// p - Periodical
+			case "p":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// w - Updating Web site
+			case "w":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// | - No attempt to code
+			case "|":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
 				break;
 
 			default:
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
 				break;
 
 			}
 
-			// if (bibLevel == 'b' || bibLevel == 'i' || bibLevel == 's') {
-			// if (data008pos21.equals("m")) {
-			// model.add(rdfSubject, RDF.type, Constants.NS_BIBO_SERIES);
-			// } else {
-			// model.add(rdfSubject, RDF.type,
-			// Constants.NS_BIBO_PERIODICAL);
-			//
-			// }
-			// }
+			break;
 
-		} else if (typeOfRecord == 'e') {
-			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_MAP);
-		} else if (typeOfRecord == 'g') {
-			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIOVISUAL);
+		default:
+			break;
 
 		}
-
 	}
 
 	// Feld 007 ist vorhanden
@@ -121,90 +222,36 @@ public class MediaType {
 		String data007pos00 = data007.substring(0, 1);
 		String data007pos01 = data007.substring(1, 2);
 
+		// Leader 06: Type of Record
 		switch (typeOfRecord) {
 
 		// a - Language material
 		case 'a':
-
-			switch (bibLevel) {
-
-			// Monographic component part
-			case 'a':
-				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_ARTICLE);
-				break;
-			// Serial component part
-			case 'b':
-				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_ISSUE);
-				break;
-			// Collection
-			case 'c':
-				break;
-			// Subunit
-			case 'd':
-				break;
-			// Integrating resource
-			case 'i':
-				break;
-			// Monograph/Item
-			case 'm':
-				break;
-			// Serial
-			case 's':
-
-				switch (data008pos21) {
-				// # - None of the following
-				case " ":
-					break;
-				// d - Updating database
-				case "d":
-					break;
-				// l - Updating loose-leaf
-				case "l":
-					break;
-				// m - Monographic series
-				case "m":
-					model.add(rdfSubject, RDF.type, Constants.NS_BIBO_SERIES);
-					break;
-				// n - Newspaper
-				case "n":
-					break;
-				// p - Periodical
-				case "p":
-					break;
-				// w - Updating Web site
-				case "w":
-					break;
-				// | - No attempt to code
-				case "|":
-					break;
-
-				default:
-					model.add(rdfSubject, RDF.type,
-							Constants.NS_BIBO_PERIODICAL);
-					break;
-
-				}
-
-				break;
-
-			default:
-				break;
-
-			}
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
 
 			break;
 		// c - Notated music
 		case 'c':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
 			break;
 		// d - Manuscript notated music
 		case 'd':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
 			break;
 		// e - Cartographic material
 		case 'e':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
 			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_MAP);
 			break;
 		// f - Manuscript cartographic material
 		case 'f':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_MAP);
 			break;
 		// g - Projected medium
 		case 'g':
@@ -212,10 +259,125 @@ public class MediaType {
 			break;
 		// i - Nonmusical sound recording
 		case 'i':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIODOCUMENT);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_MEDIATYPE_AUDIO);
+			break;
+		// j - Musical sound recording
+		case 'j':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIODOCUMENT);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_MEDIATYPE_AUDIO);
+			break;
+		// k - Two-dimensional nonprojectable graphic
+		case 'k':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_IMAGE);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+		// m - Computer file
+		case 'm':
+
+			break;
+		// o - Kit
+		case 'o':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIOVISUAL);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_ISBD_MEDIATYPE_MULTIMEDIA);
+			break;
+		// o - Medienkombination
+		case 'p':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_AUDIOVISUAL);
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_ISBD_MEDIATYPE_MULTIMEDIA);
+			break;
+		// r - Three-dimensional artifact
+		case 'r':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
+			break;
+		// r - Manuscript
+		case 't':
+			model.add(rdfSubject, DCTerms.medium,
+					Constants.NS_RDA_CARRIERTYPE_UNMEDIATED);
 			break;
 
 		default:
 			break;
+		}
+
+		switch (bibLevel) {
+
+		// Monographic component part
+		case 'a':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_ARTICLE);
+			break;
+		// Serial component part
+		case 'b':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_ISSUE);
+			break;
+		// Collection
+		case 'c':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_COLLECTION);
+			break;
+		// Subunit
+		case 'd':
+			break;
+		// Integrating resource
+		case 'i':
+			break;
+		// Monograph/Item
+		case 'm':
+			model.add(rdfSubject, RDF.type, Constants.NS_BIBO_DOCUMENT);
+			break;
+		// Serial
+		case 's':
+
+			switch (data008pos21) {
+			// # - None of the following
+			case " ":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// d - Updating database
+			case "d":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// l - Updating loose-leaf
+			case "l":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// m - Monographic series
+			case "m":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_SERIES);
+				break;
+			// n - Newspaper
+			case "n":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// p - Periodical
+			case "p":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// w - Updating Web site
+			case "w":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+			// | - No attempt to code
+			case "|":
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+
+			default:
+				model.add(rdfSubject, RDF.type, Constants.NS_BIBO_PERIODICAL);
+				break;
+
+			}
+
+			break;
+
+		default:
+			break;
+
 		}
 
 		switch (data007pos00) {
@@ -226,54 +388,56 @@ public class MediaType {
 		// Electronic resource
 		case "c":
 			switch (data007pos01) {
-			// a - Tape cartridge
-			case "a":
-				break;
-			// b - Chip cartridge
-			case "b":
-				break;
-			// c - Computer optical disc cartridge
-			case "c":
-				break;
-			// d - Computer disc, type unspecified
-			case "d":
-				break;
-			// e - Computer disc cartridge, type unspecified
-			case "e":
-				break;
-			// f - Tape cassette
-			case "f":
-				break;
-			// h - Tape reel
-			case "h":
-				break;
-			// j - Magnetic disk
-			case "j":
-				break;
-			// k - Computer card
-			case "k":
-				break;
-			// m - Magneto-optical disc
-			case "m":
-				break;
-			// o - Optical disc
-			case "o":
-				break;
+//			// a - Tape cartridge
+//			case "a":
+//				break;
+//			// b - Chip cartridge
+//			case "b":
+//				break;
+//			// c - Computer optical disc cartridge
+//			case "c":
+//				break;
+//			// d - Computer disc, type unspecified
+//			case "d":
+//				break;
+//			// e - Computer disc cartridge, type unspecified
+//			case "e":
+//				break;
+//			// f - Tape cassette
+//			case "f":
+//				break;
+//			// h - Tape reel
+//			case "h":
+//				break;
+//			// j - Magnetic disk
+//			case "j":
+//				break;
+//			// k - Computer card
+//			case "k":
+//				break;
+//			// m - Magneto-optical disc
+//			case "m":
+//				break;
+//			// o - Optical disc
+//			case "o":
+//				break;
 			// r - Remote
 			case "r":
-				model.add(rdfSubject, RDF.type, Constants.NS_RDA_CARRIERTYPE_ONLINERESOURCE);
+				model.add(rdfSubject, RDF.type,
+						Constants.NS_RDA_CARRIERTYPE_ONLINERESOURCE);
 				break;
-			// u - Unspecified
-			case "u":
-				break;
-			// z - Other
-			case "z":
-				break;
-			// | - No attempt to code
-			case "|":
-				break;
+//			// u - Unspecified
+//			case "u":
+//				break;
+//			// z - Other
+//			case "z":
+//				break;
+//			// | - No attempt to code
+//			case "|":
+//				break;
 			default:
-				model.add(rdfSubject, RDF.type, Constants.NS_RDA_CARRIERTYPE_COMPUTER);
+				model.add(rdfSubject, RDF.type,
+						Constants.NS_RDA_CARRIERTYPE_COMPUTER);
 				break;
 
 			}
@@ -290,7 +454,8 @@ public class MediaType {
 			break;
 		// Microform
 		case "h":
-			model.add(rdfSubject, RDF.type, Constants.NS_RDA_CARRIERTYPE_MICROFORM);
+			model.add(rdfSubject, RDF.type,
+					Constants.NS_RDA_CARRIERTYPE_MICROFORM);
 			break;
 		// Nonprojected graphic
 		case "k":
